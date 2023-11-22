a0de494c96ff1e7a702067cd7ca14bdea533b47d
proefra27 <proefra27@gmail.com>
2023-11-22 08:05:12 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,26 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'CBDF_EAM_09';
+  title = 'Ejercicio 09 - Interpolacion';
+  nombre = "Jose Antonio Vargas Lopez";
+  edad=24;
+  semestre = "Quinto semestre";
+  calificaciones = [5,5,10,10,5,5,5,5];
+
+  calcularPromedio(){
+    let suma=0;
+    const longitudArreglo=this.calificaciones.length;
+    for(let i=0; i<longitudArreglo;i++)
+      suma+=this.calificaciones[i];
+    return suma/longitudArreglo;
+  }
+
+  estadoAlumno(){
+    if(this.calcularPromedio()>=6)
+      return "Aprobado";
+    else
+      return "No Aprobado";
+  }
 }
